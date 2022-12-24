37f6d0a037ca9890c2896ce3605439b10f3f11b4
ankit-kumar-098 <113231267+ankit-kumar-098@users.noreply.github.com>
2022-12-24 16:52:21 +0530

    Update Min_Heap.c

--- Min_Heap.c
+++ Min_Heap.c
@@ -54,7 +54,7 @@ int main(int argc, char const *argv[])
     scanf("%d", &n);
     A = generateArray(n); 
     printArray(A, n);
-    makeMinHeap(A, n);
+    createMinHeap(A, n);
 
     printArray(A, n);
     free(A);
